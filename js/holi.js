0f9b819f481613c30f98c71d99c62c4282d0e92c
eishikahoque <eishika.hoque@gmail.com>
2020-04-27 16:40:19 -0400

    fix: fixed the fog animation

--- js/holi.js
+++ js/holi.js
@@ -1,52 +1,5 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-
   const sceneEl = document.querySelector('a-scene');
-  const auntyEl = document.querySelector('#aunty');
-  const boyEl = document.querySelector('#boy');
-  const couple1El = document.querySelector('#couple1');
-  const coupleJumpingEl = document.querySelector('#couple-jumping');
-  const couple2El = document.querySelector('#couple2');
-  const familyEl = document.querySelector('#family');
-  const guy1El = document.querySelector('#guy1');
-  const girl1El = document.querySelector('#girl1');
-  const excitedEl = document.querySelector('#excited');
-
-  auntyEl.addEventListener('mouseenter', () => {
-    setFogColor('#22B2E7');
-  });
-
-  boyEl.addEventListener('mouseenter', () => {
-    setFogColor('#F51367');    
-  });
-
-  couple1El.addEventListener('mouseenter', () => {
-    setFogColor('#FCB32C');    
-  });
-
-  coupleJumpingEl.addEventListener('mouseenter', () => {
-    setFogColor('#F96825');    
-  });
-
-  couple2El.addEventListener('mouseenter', () => {
-    setFogColor('#9FE23B');    
-  });
-
-  familyEl.addEventListener('mouseenter', () => {
-    setFogColor('#26D1AA');    
-  });
-
-  guy1El.addEventListener('mouseenter', () => {
-    setFogColor('#BD4BD7');    
-  });
-
-  girl1El.addEventListener('mouseenter', () => {
-    setFogColor('#A61265');    
-  });
-
-  excitedEl.addEventListener('mouseenter', () => {
-    setFogColor('#B4EFFB');    
-  });
 
   const setFogColor = (color) => {
     sceneEl.setAttribute('fog', {
@@ -56,5 +9,56 @@ document.addEventListener("DOMContentLoaded", () => {
       near: 0
     });
     sceneEl.emit('showFog');
-  }
+  };
+
+  document.getElementById('introBtn').addEventListener('mouseenter', () => {
+
+    const auntyEl = document.querySelector('#aunty');
+    const boyEl = document.querySelector('#boy');
+    const couple1El = document.querySelector('#couple1');
+    const coupleJumpingEl = document.querySelector('#couple-jumping');
+    const couple2El = document.querySelector('#couple2');
+    const familyEl = document.querySelector('#family');
+    const guy1El = document.querySelector('#guy1');
+    const girl1El = document.querySelector('#girl1');
+    const excitedEl = document.querySelector('#excited');
+    
+    auntyEl.addEventListener('mouseenter', () => {
+      setFogColor('#22B2E7');
+    });
+    
+    boyEl.addEventListener('mouseenter', () => {
+      setFogColor('#F51367');    
+    });
+    
+    couple1El.addEventListener('mouseenter', () => {
+      setFogColor('#FCB32C');    
+    });
+    
+    coupleJumpingEl.addEventListener('mouseenter', () => {
+      setFogColor('#F96825');    
+    });
+    
+    couple2El.addEventListener('mouseenter', () => {
+      setFogColor('#9FE23B');    
+    });
+    
+    familyEl.addEventListener('mouseenter', () => {
+      setFogColor('#26D1AA');    
+    });
+    
+    guy1El.addEventListener('mouseenter', () => {
+      setFogColor('#BD4BD7');    
+    });
+    
+    girl1El.addEventListener('mouseenter', () => {
+      setFogColor('#A61265');    
+    });
+    
+    excitedEl.addEventListener('mouseenter', () => {
+      setFogColor('#B4EFFB');    
+    });
+    
+  });
+
 });
